da3ec941099817eba56ea52f29e01616dfdca786
Bashir Muhammed <bashmufol@gmail.com>
2025-07-24 10:22:02 +0100

    MenuItem class created

---
+++ src/main/java/com/bash/mealflow/model/MenuItem.java
@@ -0,0 +1,26 @@
+package com.bash.mealflow.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "menu_items")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private LocalDate availableDate;
+    private boolean isAvailable;
+}
