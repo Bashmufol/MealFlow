8cc0b6fac1a6325cfdf902e98649051db829a0b9
Bashir Muhammed <bashmufol@gmail.com>
2025-07-24 08:30:21 +0100

    My first commit

---
+++ src/main/java/com/bash/mealflow/MealFlowApplication.java
@@ -0,0 +1,13 @@
+package com.bash.mealflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bash/mealflow/MealFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bash.mealflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
