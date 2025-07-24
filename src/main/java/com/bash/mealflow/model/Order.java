fa37b09e7b34791f5f8727147e44616d5173d30b
Bashir Muhammed <bashmufol@gmail.com>
2025-07-24 15:41:44 +0100

    updated

---
+++ src/main/java/com/bash/mealflow/model/Order.java
@@ -0,0 +1,29 @@
+package com.bash.mealflow.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id", nullable = false)
+    private User user;
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+    private String status;
+    @Column(name = "total_amount")
+    private BigDecimal totalAmount;
+    @OneToMany(mappedBy = "order",  cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+
+
+
+}
