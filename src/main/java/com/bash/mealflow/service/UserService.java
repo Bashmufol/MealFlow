7ac9a7f2fff92a35a8fa1f0b1ce61973043fe8c4
Bashir Muhammed <bashmufol@gmail.com>
2025-07-25 10:22:42 +0100

    UserService completed

--- src/main/java/com/bash/mealflow/service/UserService.java
+++ src/main/java/com/bash/mealflow/service/UserService.java
@@ -1,4 +1,21 @@
 package com.bash.mealflow.service;
 
+import com.bash.mealflow.model.User;
+import com.bash.mealflow.repository.UserRepositroy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepositroy userRepositroy;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepositroy.findByUsername(username);
+    }
+    public User saveUser(User user) {
+        return userRepositroy.save(user);
+    }
 }
