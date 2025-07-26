aec9435f3ba686792ca56569e70ab78307403a63
Bashir Muhammed <bashmufol@gmail.com>
2025-07-26 14:04:45 +0100

    finished AuthController

---
+++ src/main/java/com/bash/mealflow/controller/AuthController.java
@@ -0,0 +1,21 @@
+package com.bash.mealflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/access-denied")
+    public String accessDenied(){
+        return "access-denied";
+    }
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/dashboard";
+    }
+}
