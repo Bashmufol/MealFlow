e451cc42b543c38e8b609308456803edda09fdcc
Bashir Muhammed <bashmufol@gmail.com>
2025-07-27 13:02:50 +0100

    updated

--- src/main/java/com/bash/mealflow/controller/UserController.java
+++ src/main/java/com/bash/mealflow/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -41,8 +42,34 @@ public class UserController {
     @PostMapping("/place-order")
     public String placeOrder(@AuthenticationPrincipal UserPrincipal currentUserPrincipal,
                              @RequestParam Map<String, String> formData,
-                             RedirectAttributes redirectAttributes){
+                             RedirectAttributes redirectAttributes) {
+        User currentUser = currentUserPrincipal.getUser();
+        if (currentUser == null) {
+            redirectAttributes.addFlashAttribute("errorMessage", "User not found");
+            return "redirect:/user/dashboard";
+        }
 
+        Map<Long, Integer> itemQuantities = new HashMap<>();
+        for (Map.Entry<String, String> entry : formData.entrySet()) {
+            if (entry.getKey().startsWith("quantity_") && !entry.getValue().isEmpty()) {
+                try {
+                    Long menuItemId = Long.parseLong(entry.getKey().replace("quantity_", ""));
+                    Integer quantity = Integer.parseInt(entry.getValue());
+                    if(quantity>0){
+                        itemQuantities.put(menuItemId, quantity);
+                    }
+                } catch(NumberFormatException e){
+                    redirectAttributes.addFlashAttribute("errorMessage", "Invalid quantity for an item.");
+                    return "redirect:/user/dashboard";
+                }
+            }
+        }
+        try{
+            orderService.placeOrder(currentUser, itemQuantities);
+            redirectAttributes.addFlashAttribute("successMessage", "Order placed successfully");
+        } catch(Exception e){
+            redirectAttributes.addFlashAttribute("errorMessage", "Error placing order. " + e.getMessage());
+        }
+        return "redirect:/user/dashboard";
     }
-
 }
