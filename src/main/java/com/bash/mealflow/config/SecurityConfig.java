1afbb28adb2541da2d30ae70dedfe45d5d41418f
Bashir Muhammed <bashmufol@gmail.com>
2025-08-08 18:59:39 +0100

    updated

--- src/main/java/com/bash/mealflow/config/SecurityConfig.java
+++ src/main/java/com/bash/mealflow/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.bash.mealflow.config;
 
+import com.bash.mealflow.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,32 +18,33 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/css/**", "/js/**", "/", "/webjars/**", "/images/**", "/login", "/register").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/", "/webjars/**", "/images/**", "/login", "/register", "/oauth2/**").permitAll() // Added /oauth2/**
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login")
                         .successHandler(customAuthenticationSuccessHandler)
-//                        .defaultSuccessUrl("/user/dashboard", true)
                         .permitAll())
+                .oauth2Login(oauth2 -> oauth2 // OAuth2 Login Configuration
+                        .loginPage("/login") // Use the same login page
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)) // Custom service for handling OAuth2 user details
+                        .successHandler(customAuthenticationSuccessHandler)) // Use custom handler for OAuth2 success too
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
                         .permitAll())
                 .exceptionHandling(exception -> exception
                         .accessDeniedPage("/access-denied"))
-
                 .build();
-
-
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
